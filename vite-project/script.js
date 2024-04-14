a2acdc159ced6bfc5e8c0513d3dbfdebe2defb44
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-04-14 08:07:34 +0500

    animations

--- vite-project/script.js
+++ vite-project/script.js
@@ -1,6 +1,11 @@
-gsap.from("nav .logo-icon, nav ul",{
+
+const tl = gsap.timeline()
+
+tl.from("nav .logo-icon, nav ul li",{
     y:-100,
-    duration:0.5,
+    duration:0.3,
     delay:1,
-    opacity:0
+    opacity:0,
+    stagger:0.2
 })
+
