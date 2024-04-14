b0b9db7a3cd1d23936daccbd541e6ac33da4ed61
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-04-14 13:19:25 +0500

    animations more

--- vite-project/script.js
+++ vite-project/script.js
@@ -9,3 +9,37 @@ tl.from("nav .logo-icon, nav ul li",{
     stagger:0.2
 })
 
+tl.from("body .bg-mask",{
+    scale:0,
+    y: -500,
+    opacity:0,
+    duration:1
+})
+
+tl.from("body .bg-particles, body .bg-img",{
+    opacity:0,
+    duration:1,
+})
+
+
+const warp = gsap.timeline()
+
+warp.to("feTurbulence",{
+    duration:15,
+    repeat:-1,
+    yoyo:true,
+    ease:"Linear.easeNone",
+    attr:{
+        baseFrequency:0.01
+    }
+})
+
+warp.to("feDisplacementMap",{
+    duration:15,
+    repeat:-1,
+    yoyo:true,
+    ease:"Linear.easeNone",
+    attr:{
+        scale: 77
+    }
+}, 0)
