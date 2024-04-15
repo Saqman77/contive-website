f980cd51003c8c1faa276f68102a5136c604a3f7
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-04-15 19:24:39 +0500

    new update

--- vite-project/script.js
+++ vite-project/script.js
@@ -1,10 +1,17 @@
 
 const tl = gsap.timeline()
 
-tl.from("nav .logo-icon, nav ul li",{
+tl.from("nav .logo-icon",{
     y:-100,
     duration:0.3,
     delay:1,
+    opacity:0,
+    // stagger:0.2
+})
+tl.from("nav ul li",{
+    // y:-100,
+    // duration:0.3,
+    // delay:1,
     opacity:0,
     stagger:0.2
 })
@@ -21,15 +28,22 @@ tl.from("body .bg-img",{
     duration:1,
 })
 
-tl.from("body .bg-particles",{
-    // scale:0,
-    // y: 500,
-    opacity:0,
-    duration:2,
-    repeat:-1,
-    yoyo:true
-})
-
+tl.fromTo(
+    "body .bg-particles",
+    {
+        scale: 0.5, // Starting scale
+        y: -500,
+        opacity: 1,
+    },
+    {
+        scale: 1.7, // Ending scale
+        y: 300,
+        opacity: .5,
+        duration: 10,
+        repeat: -1,
+        ease: "Linear.easeNone",
+    }
+);
 
 const warp = gsap.timeline()
 
